7713a6f7537a1217cb1e5efcaa0df112c08ec8eb
Alexandru Cuta <alexandru.cuta@yahoo.com>
2021-06-15 23:18:06 +0300

    removed test files

--- compile_test.bat
+++
@@ -1,2 +0,0 @@
-nvcc -rdc=true %~dp0\src\memcached_test.cu -o memcached ^
-    -ccbin "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64\cl.exe"
